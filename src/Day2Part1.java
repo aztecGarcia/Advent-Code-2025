ea367c167c65606ff92b02728c8b07058489734e
Itsooo <itso.user@gmail.com>
2024-12-02 19:27:30 -0600

    Part 1 finished

---
+++ src/Day2Part1.java
@@ -0,0 +1,45 @@
+package src;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day2Part1 {
+    public static void main(String[] args) throws FileNotFoundException {
+      Scanner puzzleScan = new Scanner(new File("PuzzleInput/Day2.txt"));
+      int safe = 0;
+      int line = 0;
+
+      while (puzzleScan.hasNextLine()) {
+        line++;
+        Scanner lineScan = new Scanner (puzzleScan.nextLine());
+
+        boolean wasSafe = true;
+        int previous = lineScan.nextInt();
+        Integer previousSlope = null;
+        while (lineScan.hasNextInt()) {
+          int current = lineScan.nextInt();
+          int slope = previous - current;
+          int slopeAbs = Math.abs(slope);
+
+          if ((previousSlope != null && previousSlope * slope <= 0) || 1 > slopeAbs || slopeAbs > 3) {
+            wasSafe = false;
+            break;
+          }
+
+          
+          previous = current;
+          if (previousSlope == null) previousSlope = slope;
+        }
+        
+        if (wasSafe) safe++;
+
+        lineScan.close();
+
+        // if (line > 5) return;
+      }
+      
+      puzzleScan.close();
+
+      System.out.println(safe);
+    }
+  }
